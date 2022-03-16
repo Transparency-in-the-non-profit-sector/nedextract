6561532555c7bbccb0c7707b8f90e654485f1cd6
Laura Ootes <l.ootes@esciencecenter.nl>
2022-03-16 14:19:07 +0100

    first version setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
-import os
+
+# Copyright 2022 Netherlands eScience Center and Vrije Universiteit Amsterdam
+# Licensed under the Apache License, version 2.0. See LICENSE for details.
+
 from setuptools import setup
 
 
 # see setup.cfg
-setup()
+setup(name='run_auto_extract',
+      version='0.0.1',
+      description='automatically extract information from annual report PDF files',
+      author='Laura S. Ootes',
+      author_email='l.ootes@esciencecenter.nl',
+      packages=['auto_extract'],
+      install_requires=['numpy', 'pandas', 'stanza', 'fuzzywuzzy', 'pdftotext'],
+      license='',
+      url=''
+      )
