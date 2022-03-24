be2378eb6cf520eb3c7d80fa7e97c79622763405
Laura Ootes <l.ootes@esciencecenter.nl>
2022-03-24 14:07:32 +0100

    updated dependencies

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ from setuptools import setup
 # see setup.cfg
 setup(name='run_auto_extract',
       version='0.0.1',
-      description='automatically extract information from annual report PDF files',
+      description='automatically extract information from Dutch annual report PDF files',
       author='Laura S. Ootes',
       author_email='l.ootes@esciencecenter.nl',
       packages=['auto_extract'],
-      install_requires=['numpy', 'pandas', 'stanza', 'fuzzywuzzy', 'pdftotext'],
-      license='',
-      url=''
+      install_requires=['numpy', 'pandas', 'stanza', 'python-Levenshtein', 'fuzzywuzzy',
+                        'pdftotext', 'sklearn', 'openpyxl'],
+      license='Apache Software License',
+      url='https://github.com/Transparency-in-the-non-profit-sector/np-transparency'
       )
