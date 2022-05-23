02bd82f11cb0f4046d6acb13628baf4d021b8b95
Laura Ootes <l.ootes@esciencecenter.nl>
2022-05-23 22:12:18 +0200

    add additional orgs tests

--- tests/test_extract_related_orgs.py
+++ tests/test_extract_related_orgs.py
@@ -1,13 +1,18 @@
 import os
 import numpy as np
+import pandas as pd
 import stanza
+from auto_extract.extract_related_orgs import apply_matching
 from auto_extract.extract_related_orgs import check_single_orgs
 from auto_extract.extract_related_orgs import collect_orgs
+from auto_extract.extract_related_orgs import count_number_of_mentions
 from auto_extract.extract_related_orgs import decide_org
 from auto_extract.extract_related_orgs import keyword_check
+from auto_extract.extract_related_orgs import match_anbis
 from auto_extract.extract_related_orgs import part_of_other
 from auto_extract.extract_related_orgs import percentage_considered_org
 from auto_extract.extract_related_orgs import single_org_check
+from auto_extract.extract_related_orgs import strip_function_of_entity
 from auto_extract.preprocessing import preprocess_pdf
 
 
@@ -115,3 +120,53 @@ def test_pco():
     pco = percentage_considered_org(doc, org, orgs, counts)
     assert(pco[0] == -10.)
     assert(pco[1] == 0)
+
+
+def test_strip_function_of_entity():
+    org = "Lid van de Raad van Advies bij Bedrijfsnaam b.v."
+    e_org = "Bedrijfsnaam b.v."
+    o_org = strip_function_of_entity(org)
+    assert(e_org == o_org)
+
+
+def test_count_number_of_mentions():
+    org = 'Bedrijf'
+    n = count_number_of_mentions(doc, org)
+    assert(n == 7)
+    org = 'Bedrijf-'
+    n = count_number_of_mentions(doc, org)
+    assert(n == 0)
+
+
+def test_apply_matching():
+    df = pd.DataFrame({'currentStatutoryName': ['Bedrijf1', 'Bedrijf2'],
+                       'shortBusinessName': ['B1 b.v.', 'B2 b.v.']})
+    m = 'Bedrijf2'
+    o_m = apply_matching(df, m, 'currentStatutoryName', 'shortBusinessName')
+    e_m = 'Bedrijf2'
+    assert(o_m == e_m)
+    m = 'Bedrijf'
+    o_m = apply_matching(df, m, 'currentStatutoryName', 'shortBusinessName')
+    e_m = None
+    assert(o_m == e_m)
+    df = pd.DataFrame({'currentStatutoryName': ['Bedrijf1', 'Bedrijf2'],
+                       'shortBusinessName': ['Stichting B1 b.v.', 'B2 b.v.']})
+    m = 'B1 b.v.'
+    o_m = apply_matching(df, m, 'currentStatutoryName', 'shortBusinessName')
+    e_m = 'Stichting B1 b.v.'
+    assert(o_m == e_m)
+
+def test_match_anbis():
+    anbis_file = os.path.join(os.path.join(os.getcwd(), 'tests'), 'test_anbis.csv')
+    df_in = pd.DataFrame({'Input_file': ['test.pdf'],
+                          'mentioned_organization': ['B1 b.v.'],
+                          'n_mentions': [1]})
+    df_out = match_anbis(df_in, anbis_file)
+    e_out = pd.DataFrame({'Input_file': ['test.pdf'],
+                          'mentioned_organization': ['B1 b.v.'],
+                          'n_mentions': [1],
+                          'matched_anbi': ['Stichting B1 b.v.'],
+                          'rsin': ['11'],
+                          'currentStatutoryName':['Bedrijf1'],
+                          'shortBusinessName':['Stichting B1 b.v.']})
+    pd.testing.assert_frame_equal(df_out, e_out)
