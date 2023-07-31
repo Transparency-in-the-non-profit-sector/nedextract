d9550a7edfa52351bc5281a1c295db7ff856d5e8
Laura Ootes <l.ootes@esciencecenter.nl>
2023-07-31 14:45:34 +0200

    remove out-of-scope file

---
+++ auto_extract/extract_SDG.py
@@ -0,0 +1,23 @@
+# Copyright 2022 Netherlands eScience Center and Vrije Universiteit Amsterdam
+# Licensed under the Apache License, version 2.0. See LICENSE for details.
+
+# Determine to which Sustainable Development Goals (SDG) a organization contributes.
+#
+# De doelen zijn:
+# Uitbannen van alle vormen van (extreme) armoede
+# Einde aan honger, zorgen voor voedselzekerheid en duurzame landbouw
+# Gezondheidszorg voor iedereen
+# Inclusief, gelijkwaardig en kwalitatief onderwijs voor iedereen
+# Gelijke rechten voor mannen en vrouwen en empowerment van vrouwen en meisjes
+# Schoon water en sanitaire voorzieningen voor iedereen
+# Toegang tot betaalbare en duurzame energie voor iedereen
+# Inclusieve, economische groei, werkgelegenheid en fatsoenlijk werk voor iedereen
+# Infrastructuur voor duurzame industrialisatie
+# Verminderen ongelijkheid binnen en tussen landen
+# Maak steden veilig, veerkrachtig en duurzaam
+# Duurzame consumptie en productie
+# Aanpak klimaatverandering
+# Beschermen en duurzaam gebruik van de oceanen en zeeën
+# Beschermen van ecosystemen, bossen en biodiversiteit
+# Bevorderen van veiligheid, publieke diensten en recht voor iedereen
+# Versterken van het mondiaal partnerschap om doelen te bereiken
