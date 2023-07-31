8de5edf2bf2c22a7e3e8e540b341d9366e1f64c4
Laura Ootes <l.ootes@esciencecenter.nl>
2023-07-31 14:43:11 +0200

    add docstring

--- tests/test_run_auto_extract.py
+++ tests/test_run_auto_extract.py
@@ -1,13 +1,25 @@
+"""Tests for run_auto_extract"""
 import os
 from auto_extract.run_auto_extract import main
 
 
 def test_main():
+    """
+    Unit test function for the 'main' function.
+
+    It checks two scenarios:
+    1. Testing with a file argument (-f option), using test file: tests/test_report.pdf
+    2. Testing with a directory argument (-d option), using test directory: tests
+
+    Raises:
+        AssertionError: If any of the assert statements fail, indicating incorrect return values.
+    """
+    
     indir = os.path.join(os.getcwd(), 'tests')
     infile = os.path.join(indir, 'test_report.pdf')
     testarg = ['-f', infile]
     returnargs = main(testarg)
-    assert(returnargs.file == infile)
+    assert returnargs.file == infile
     testarg = ['-d', indir]
     returnargs = main(testarg)
-    assert(returnargs.directory == indir)
+    assert returnargs.directory == indir
