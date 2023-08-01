a5c3bf73e8ee5db708b0271675bdadce8b27b1b7
Laura Ootes <l.ootes@esciencecenter.nl>
2023-08-01 16:15:06 +0200

    use unittest framework

--- tests/test_run_auto_extract.py
+++ tests/test_run_auto_extract.py
@@ -1,23 +1,33 @@
 """Tests for run_auto_extract."""
+import unittest
 import os
 from auto_extract.run_auto_extract import main
 
 
-def test_main():
-    """Unit test function for the 'main' function.
+class TestRunAutoExtract(unittest.TestCase):
+    def test_main(self):
+        """Unit test function for the 'main' function.
 
-    It checks two scenarios:
-    1. Testing with a file argument (-f option), using test file: tests/test_report.pdf
-    2. Testing with a directory argument (-d option), using test directory: tests
+        It checks two scenarios:
+        1. Testing with a file argument (-f option), using test file: tests/test_report.pdf
+        2. Testing with a directory argument (-d option), using test directory: tests
 
-    Raises:
-        AssertionError: If any of the assert statements fail, indicating incorrect return values.
-    """
-    indir = os.path.join(os.getcwd(), 'tests')
-    infile = os.path.join(indir, 'test_report.pdf')
-    testarg = ['-f', infile]
-    returnargs = main(testarg)
-    assert returnargs.file == infile
-    testarg = ['-d', indir]
-    returnargs = main(testarg)
-    assert returnargs.directory == indir
+        Raises:
+            AssertionError: If any of the assert statements fail, indicating incorrect return values.
+        """
+        indir = os.path.join(os.getcwd(), 'tests')
+        infile = os.path.join(indir, 'test_report.pdf')
+
+        # Test case 1
+        testarg = ['-f', infile]
+        returnargs = main(testarg)
+        self.assertEqual(returnargs.file == infile)
+
+        # Test case 2
+        testarg = ['-d', indir]
+        returnargs = main(testarg)
+        self.assertEqual returnargs.directory == indir
+
+
+if __name__ == '__main__':
+    unittest.main()
