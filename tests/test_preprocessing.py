ca25a161009aa2c8d69ccf38dc5a03678b5f10c3
Laura Ootes <l.ootes@esciencecenter.nl>
2023-08-04 14:21:52 +0200

    convert to class and add docstrings

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,26 +1,46 @@
+import unittest
 import os
 from auto_extract.preprocessing import delete_downloaded_pdf
 from auto_extract.preprocessing import download_pdf
 from auto_extract.preprocessing import preprocess_pdf
 
 
-def test_preprocess_pdf():
-    indir = os.path.join(os.getcwd(), 'tests')
-    infile = os.path.join(indir, 'test_report.pdf')
-    text = preprocess_pdf(infile, ' ')
-    text = preprocess_pdf(infile, '. ')
-    text = preprocess_pdf(infile, ', ')
-    assert(isinstance(text, str))
+class UnitTestsPreprocessing(unittest.TestCase):
+    """Unit test class for testing functions used to preprocess text.
+    
+    Contains:
+    - test_preprocess_pdf
+    - test_download_pdf
+    - test_delete_pdf"""
+
+    def test_preprocess_pdf():
+        """Unit test for the function preprocess_pdf.
+        
+        The function tests the preprocess_pdf function that takes in a PDF file, reads the text content,
+        and performs several text preprocessing steps to clean and format the text."""
+        indir = os.path.join(os.getcwd(), 'tests')
+        infile = os.path.join(indir, 'test_report.pdf')
+        text = preprocess_pdf(infile, ' ')
+        text = preprocess_pdf(infile, '. ')
+        text = preprocess_pdf(infile, ', ')
+        assert(isinstance(text, str))
 
 
-def test_download_pdf():
-    url = ("https://github.com/Transparency-in-the-non-profit-sector/" +
-           "np-transparency/blob/main/tests/test_report.pdf")
-    filename = download_pdf(url)
-    assert(os.path.exists(filename))
+    def test_download_pdf():
+        """Unit test for the function download_pdf.
+        
+        This function tests the download_pdf dunction that downloads a pdf file from an url and safe it in the cwd."""
+        url = ("https://github.com/Transparency-in-the-non-profit-sector/" +
+            "np-transparency/blob/main/tests/test_report.pdf")
+        filename = download_pdf(url)
+        assert(os.path.exists(filename))
 
 
-def test_delete_downloaded_pdf():
-    delete_downloaded_pdf()
-    filename = os.path.join(os.getcwd(), "downloaded.pdf")
-    assert(os.path.exists(filename) is False)
+    def test_delete_downloaded_pdf():
+        """Unit test for the function delete_downloaded_pdf.
+        
+        This function tests the funciton delete_downloaded pdf that deletes the file that is downloaded with the function
+        download_pdf and saved as downloaded.pdf from the cwd."""
+        delete_downloaded_pdf()
+        filename = os.path.join(os.getcwd(), "downloaded.pdf")
+        assert(os.path.exists(filename) is False)
