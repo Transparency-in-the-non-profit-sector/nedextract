594f6cdf326898c26a691a80ee984e06f601b81b
Laura Ootes <l.ootes@esciencecenter.nl>
2023-08-08 11:49:48 +0200

    fix org versus organisation

--- auto_extract/extract_related_orgs.py
+++ auto_extract/extract_related_orgs.py
@@ -70,10 +70,10 @@ def collect_orgs(infile: str, nlp: stanza.Pipeline):
             single_orgs.append(n_org)
         else:
             extraction.doc = doc_c
-            extraction.orgs = org_c
+            extraction.organisation = org_c
             pco_c = extraction.percentage_considered_org()
             extraction.doc = doc_p
-            extraction.orgs = org_p
+            extraction.organisation = org_p
             pco_p = extraction.percentage_considered_org()
             pco = pco_c, pco_p
             decision = decide_org(org, pco, org_pp, org_c, nlp)
