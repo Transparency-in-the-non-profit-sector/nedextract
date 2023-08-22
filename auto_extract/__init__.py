7867297ab01c99da0043f9f9a504c72a50252762
Laura Ootes <l.ootes@esciencecenter.nl>
2023-08-22 17:18:39 +0200

    move files

--- auto_extract/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Basic info about auto_extract."""
-import logging
-
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Laura Ootes"
-__email__ = "l.ootes@esciencecenter.nl"
-__version__ = "0.1.0"
