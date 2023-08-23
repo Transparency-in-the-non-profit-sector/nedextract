efed8a9e58f4c549b8b13977eaf0d2585be4d699
Laura Ootes <l.ootes@esciencecenter.nl>
2023-08-23 16:55:33 +0200

    correct argument naming and add docstring

--- nedextract/run_nedextract.py
+++ nedextract/run_nedextract.py
@@ -8,10 +8,8 @@ Copyright 2022 Netherlands eScience Center
 Licensed under the Apache License, version 2.0. See LICENSE for details.
 """
 
-import argparse
 import os
 import time
-from argparse import RawTextHelpFormatter
 from datetime import datetime
 import numpy as np
 import pandas as pd
@@ -21,7 +19,7 @@ from nedextract.preprocessing import download_pdf
 from nedextract.read_pdf import PDFInformationExtractor
 
 
-def run(dir=None, file=None, url=None, urlf=None, tasks=['people'], anbis=None, model=None, labels=None,
+def run(directory=None, file=None, url=None, urlf=None, tasks=['people'], anbis=None, model=None, labels=None,
         vectors=None, write_output=False):
     """Annual report information extraction.
 
@@ -43,7 +41,7 @@ def run(dir=None, file=None, url=None, urlf=None, tasks=['people'], anbis=None, 
     - The gathered information is converted into a pandas DataFrame and optionally saved to excel files.
 
     Args:
-        dir (str): Directory containing annual reports to be processed. 
+        directory (str): Directory containing annual reports to be processed. 
         file (str): Path to specific pdf file to be processed.
         url (str): URL to pdf file to be processed.
         urlf (str): txt file containing url paths to be processed.
@@ -60,7 +58,7 @@ def run(dir=None, file=None, url=None, urlf=None, tasks=['people'], anbis=None, 
     # start time
     start_time = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
 
-    if not (dir or file or url or urlf):
+    if not (directory or file or url or urlf):
         raise FileNotFoundError('No input provided. Run with -h for help on arguments to be provided.')
 
     # Create the output directory if it does not exist already
@@ -81,24 +79,24 @@ def run(dir=None, file=None, url=None, urlf=None, tasks=['people'], anbis=None, 
     if file:
         infile = os.path.join(os.getcwd(), file)
         opd_p, opd_g, opd_o = pdf_extractor.extract_pdf(infile, opd_p, opd_g, opd_o)
-    elif dir:
-        totalfiles = len([name for name in os.listdir(os.path.join(os.getcwd(), dir))
+    elif directory:
+        totalfiles = len([name for name in os.listdir(os.path.join(os.getcwd(), directory))
                          if name.lower().endswith('.pdf')])
-        for filename in os.listdir(os.path.join(os.getcwd(), args.directory)):
+        for filename in os.listdir(os.path.join(os.getcwd(), directory)):
             if filename.lower().endswith('.pdf'):
                 countfiles += 1
                 print('Working on file:', countfiles, 'out of', totalfiles)
-                infile = os.path.join(os.getcwd(), dir, filename)
+                infile = os.path.join(os.getcwd(), directory, filename)
                 opd_p, opd_g, opd_o = pdf_extractor.extract_pdf(infile, opd_p, opd_g, opd_o)
     elif url:
         infile = download_pdf(url)
         opd_p, opd_g, opd_o = pdf_extractor.extract_pdf(infile, opd_p, opd_g, opd_o)
         delete_downloaded_pdf()
-    elif args.url_file:
-        with open(args.url_file, mode='r', encoding='UTF-8') as u:
+    elif urlf:
+        with open(urlf, mode='r', encoding='UTF-8') as u:
             urls = u.readlines()
-        for url in urls:
-            print(url)
+        for urlp in urls:
+            print(urlp)
             infile = download_pdf(url)
             opd_p, opd_g, opd_o = pdf_extractor.extract_pdf(infile, opd_p, opd_g, opd_o, anbis)
             delete_downloaded_pdf()
@@ -116,10 +114,19 @@ def run(dir=None, file=None, url=None, urlf=None, tasks=['people'], anbis=None, 
 
 def output_to_df(opd_p=None, opd_g=None, opd_o=None, anbis_file=None):
     """
-    Input
+    Convert extracted data in numpy arrays to pandas dataframes with correct column names.
+
+    Args:
+        opd_p (numpy.ndarray): A numpy array containing output for people mentioned in pdf.
+        opd_g (numpy.ndarray): A numpy array containing predicted sector in a pdf.
+        opd_o (numpy.ndarray): A numpy array containing related organizations mentioned in a pdf.
 
     Returns
+        df_p, df_g, df_o: three pd.DataFrames containing the input information on people, sectors,
+        and organizations repectively
     """
+    df_p, df_g, df_o = pd.DataFrame
+
     if opd_p:
         cols_p = ['Input_file', 'Organization', 'Persons', 'Ambassadors',
                   'Board_members', 'Job_description']
@@ -130,7 +137,6 @@ def output_to_df(opd_p=None, opd_g=None, opd_o=None, anbis_file=None):
         cols_p.extend([f'kascommissie{n}' for n in range(1, 6)])
         cols_p.extend([f'controlecommissie{n}' for n in range(1, 6)])
         df_p = pd.DataFrame(opd_p, columns=cols_p)
-        
 
     # Convert sectors to df
     if opd_g:
@@ -149,14 +155,13 @@ def output_to_df(opd_p=None, opd_g=None, opd_o=None, anbis_file=None):
 def write_output(tasks: list, dfp: pd.DataFrame, dfg: pd.DataFrame, dfo: pd.DataFrame):
     """Write extracted information to output files.
 
-    Create three output files for people, sectors, and organisations.
+    Create three output excel files for people, sectors, and organisations.
 
     Args:
         tasks (list): list of arguments used to define which tasks had to be executed
         dfp (pd.DataFrame): output results df for people task
         dfg (pd.DataFrame): output results df for sectors tasks
         dfo (pd.DataFrame): output results df for organisations task
-    Returns:
     """
     outtime = time.strftime("%Y%m%d_%H%M%S", time.localtime())
 
