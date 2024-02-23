48e9eae0e56ffbc8ffbd396a25665c13eaf6c6b7
Laura Ootes <l.ootes@esciencecenter.nl>
2024-02-23 10:55:24 +0100

    add init file

---
+++ nedextract/utils/__init__.py
@@ -0,0 +1,9 @@
+"""Basic info about nedextract."""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Laura Ootes"
+__email__ = "l.ootes@esciencecenter.nl"
+__version__ = "0.2.0"
